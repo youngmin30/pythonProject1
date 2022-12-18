41f9301c132f434796ce896d053ae30b8de55068
youngmin30 <jjymjj@naver.com>
2022-12-18 13:51:47 +0900

    code 082

---
+++ code082.py
@@ -0,0 +1,8 @@
+# by min
+# 082 Verifying certain character in string set
+
+msg = input("임의의 문장을 입력하세요: ")
+if 'is' in msg:
+    print("당신이 입력한 문장에는 is가 있습니다.")
+else:
+    print("당신이 입력한 문장에는 is가 없습니다.")
