411440fc4d643f6b8cd9cc45700a6022b293b0e7
youngmin30 <jjymjj@naver.com>
2022-12-18 13:47:06 +0900

    Initial commit

---
+++ code081.py
@@ -0,0 +1,9 @@
+# by jym
+# 초보자를 위한 파이썬 200제
+# 081 Verifying certain character in string set
+
+msg = input("임의의 문장을 입력하세요:")
+if 'a' in msg:
+    print("당신이 입력한 문장에는 a가 있습니다.")
+else:
+    print("당신이 입력한 문장에는 a가 없습니다.")
