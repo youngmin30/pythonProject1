4da7dbf60a9aa652f7848d6545a64ffa171ae4da
youngmin30 <jjymjj@naver.com>
2022-12-21 17:51:55 +0900

    code 083 알파벳 검사 / 일종의 인코딩 확인

---
+++ code084.py
@@ -0,0 +1,15 @@
+# code 084
+
+txt1 = 'A' # 알파벳
+txt2 = '안녕' # 한글
+txt3 = 'Warcraft Three' # 영문 대소문자
+txt4 = '3PO' # 숫자, 대문자
+
+ret1 = txt1.isalpha()
+ret2 = txt2.isalpha()
+ret3 = txt3.isalpha()
+ret4 = txt4.isalpha()
+print(ret1) # 결과는 True
+print(ret2) # 결과는 True
+print(ret3) # False
+print(ret4) # False
