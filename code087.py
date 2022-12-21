ed9eba4bed2b044cdbde92e0a61a0eb0e7b8dee4
youngmin30 <jjymjj@naver.com>
2022-12-21 18:27:12 +0900

    code 087 대소문자 변환(upper, lower)

--- code087.py
+++ code087.py
@@ -1,7 +1,7 @@
 # 087
 
 txt = 'A lot of Things occur each day.'
-ret1 = txt.upper()
-ret2 = txt.lower()
+ret1 = txt.upper() # txt에 있는 문장을 대문자로 변환하기
+ret2 = txt.lower() # txt에 있는 문장을 다시 소문자로 변환하기
 print(ret1)
 print(ret2)
