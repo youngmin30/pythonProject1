b6fe0a9a86ea56287d60b7e6283920d3f3a07d3e
youngmin30 <jjymjj@naver.com>
2022-12-21 18:05:20 +0900

    code 085 문자열이 숫자인지 검사

--- code085.py
+++ code085.py
@@ -6,6 +6,6 @@ txt3 = '1212'
 ret1 = txt1.isdigit()
 ret2 = txt2.isdigit()
 ret3 = txt3.isdigit()
-print(ret1) # False
-print(ret2) # False
-print(ret3) # True
+print(ret1) # False 숫자로 보이는 전화번호는 숫자가 아닌 것으로 나옴
+print(ret2) # False 숫자, 영문 대문자
+print(ret3) # True 숫자
