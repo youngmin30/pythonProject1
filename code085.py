c15a13d3ddbf0982e70878647ecaaf897d6696ce
youngmin30 <jjymjj@naver.com>
2022-12-21 18:04:41 +0900

    code 085 문자열이 숫자인지 검사

---
+++ code085.py
@@ -0,0 +1,11 @@
+# 085
+
+txt1 = '010-1234-5678'
+txt2 = 'R2D2'
+txt3 = '1212'
+ret1 = txt1.isdigit()
+ret2 = txt2.isdigit()
+ret3 = txt3.isdigit()
+print(ret1) # False
+print(ret2) # False
+print(ret3) # True
